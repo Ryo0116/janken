aea61d168a0695330dfe3a39e127c06a02a6288a
Ryo0116 <ryo0116kine@gmail.com>
2022-04-04 03:25:02 +0900

    挙動の修正

--- janken.rb
+++ janken.rb
@@ -1,113 +1,85 @@
-loop do
-    puts "最初はグー、じゃんけん・・・\n0(グー) 1(チョキ) 2(パー) 3(逃げる)"
-    janken_type = gets.to_s.chomp
-    puts "==============="
+def janken
+    puts "最初はグー...\nじゃんけん..."
+    puts "0(グー)1(チョキ)2(パー)3(辞退)"
+            
+    my_hand = gets.to_i
+    cpu_hand = rand(3)
+        
+    jankens = ["グー","チョキ","パー"]
+    puts "ホイ!\n ------------------"
+        
+    win = (my_hand == 0 && cpu_hand ==1) ||(my_hand == 1 && cpu_hand ==2) || (my_hand == 2 && cpu_hand ==0)
+    lose = (my_hand == 0 && cpu_hand ==2) ||(my_hand == 1 && cpu_hand ==0) || (my_hand == 2 && cpu_hand ==1)
+        
+    if my_hand == cpu_hand
+      puts "あなた:#{jankens[my_hand]}を出しました\n 相手:#{jankens[cpu_hand]}を出しました\n------------------"
+      puts "あいこで..."
+      return true
+            
+    elsif win
+      puts "あなた:#{jankens[my_hand]}を出しました\n 相手:#{jankens[cpu_hand]}を出しました\n------------------"
+      puts "あなた：勝ち"
+      @result = "win"
+      return false
+    elsif lose
+      puts "あなた:#{jankens[my_hand]}を出しました\n 相手:#{jankens[cpu_hand]}を出しました\n------------------"
+      puts "あなた：負け"
+      @result = "lose"
+      return false
+    elsif player_hand == 3
+      puts "バイバイ!"
+      exit
+    elsif player_hand >= 4
+      puts "入力値が正しくありません。0~３を入力してください。"
+      return true
+    end
+    end
+        
+    def hoi
+    if @result == "win"
+    puts "あっち向いて〜\n0(上)1(下)2(左)3(右)"
+      
+    my_hoi = gets.to_i
+    cpu_hoi = rand(4)
+          
+    hois = ["上","下","左","右"]
+    puts "ホイ！\n------------------"
+    puts "あなた:#{hois[my_hoi]}を指しました\n相手:#{hois[cpu_hoi]}を向きました\n------------------"
+    if my_hoi == cpu_hoi
+       puts "あなたの勝ち!"
+       exit
+    elsif my_hoi != cpu_hoi
+       puts "じゃんけんからやり直し"
+       return true
+    end
+    end
+        
+    if @result == "lose"
+    puts "あっち向いて〜\n0(上)1(下)2(左)3(右)"
+          
+    my_hoi = gets.to_i
+    cpu_hoi = rand(4)
+          
+    hois = ["上","下","左","右"]
+    puts "ホイ！\n------------------"
+    puts "相手:#{hois[cpu_hoi]}を指しました\nあなた:#{hois[my_hoi]}を向きました\n------------------"
+    if my_hoi == cpu_hoi
+       puts "あなたの負け!"
+       exit
+    elsif my_hoi != cpu_hoi
+       puts "じゃんけんからやり直し"
+       return true
+    end
+    end
+    end
     
-    case janken_type
-    when "0"
-        puts "あなた：グーを出しました。"
-    when "1"
-        puts "あなた：チョキを出しました。"
-    when "2"
-        puts "あなた：パーを出しました。"
-    when "3"
-        puts "あなた：逃げました。"
-        break
-    else
-        puts "0~3を半角で入力してください。"
+    next_game2 = true
+    
+    while next_game2
+        next_game = true
+        
+        while next_game
+         next_game = janken
+        end
+      next_game = hoi
     end
-
-    r = Random.new()
-    cpu_janken_type = r.rand(3).to_s
-    case cpu_janken_type
-    when "0"
-        puts "相手：グーを出しました。"
-    when "2"
-        puts "相手：チョキを出しました。"
-    when "3"
-        puts "相手：パーを出しました。"
-    end
-    puts "==============="
-
-
-    #勝ち
-    if janken_type ==="0" && cpu_janken_type ==="1" || janken_type ==="1" && cpu_janken_type ==="2" || janken_type ==="2" && cpu_janken_type ==="0"
-        puts "あなたはじゃんけんに勝ちました。"
-        puts "あっち向いてホイ"
-        puts "0(上) 1(下) 2(左) 3(右)"
-        finger_direction_type = gets.to_s.chomp
-        puts "==============="
-
-        case finger_direction_type
-        when "0"
-            puts "上！"
-        when "1"
-            puts "下！"
-        when "2"
-            puts "左！"
-        when "3"
-            puts "右！"
-        end
-        r2 = Random.new()
-        cpu_face_direction_type = r2.rand(4).to_s
-
-        case cpu_face_direction_type
-        when "0"
-            puts "相手は上を向きました。"
-        when "1"
-            puts "相手は下を向きました。"
-        when "2"
-            puts "相手は左を向きました。"
-        when "3"
-            puts "相手は右を向きました。"
-        end
-        puts "==============="
-
-        if finger_direction_type === cpu_face_direction_type
-            puts "あなたの勝ちです。"
-            break
-        else
-        end
-
-    #負け
-    elsif janken_type ==="0" && cpu_janken_type ==="2" || janken_type ==="1" && cpu_janken_type ==="0" || janken_type ==="2" && cpu_janken_type ==="1"
-        puts "あなたはじゃんけんに負けました。"
-        puts "あっち向いてホイ！"
-        puts "0(上) 1(下) 2(左) 3(右)"
-        face_direction_type = gets.to_s.chomp
-        puts "==============="
-        case face_direction_type
-        when "0"
-            puts "あなたは上を向きました。"
-        when "1"
-            puts "あなたは下を向きました。"
-        when "2"
-            puts "あなたは左を向きました。"
-        when "3"
-            puts "あなたは右を向きました。"
-        end
-
-        r3 = Random.new()
-        cpu_face_direction_type = r3.rand(4).to_s
-
-        case cpu_face_direction_type
-        when "0"
-            puts "相手は上を指しました"
-        when "1"
-            puts "相手は下を指しました。"
-        when "2"
-            puts "相手は左を指しました。"
-        when "3"
-            puts "相手は右を指しました。"
-        end
-        puts "================"
-
-        if face_direction_type === cpu_face_direction_type
-            puts "あなたの負けです"
-            break
-        else
-        end
-    else
-    end
-
-end
